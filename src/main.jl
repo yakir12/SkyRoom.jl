abf61fcb26c7d03273ff9aed9d99ce14334d84ee
Yakir Gagnon <12.yakir@gmail.com>
2020-12-17 21:47:54 +0100

    8gb

--- src/main.jl
+++ src/main.jl
@@ -133,7 +133,7 @@ function dom_handler(sr::SkyRoom1, left2upload, session, request)
 
     md = Dict()
 
-    setup_file = HTTP.get(setupsurl["skyroom"], retry = 4).body
+    setup_file = HTTP.get(setupsurl["skyroom"]).body
     df = CSV.File(setup_file, header = 1:2, types = Dict(1 => String))  |> TableOperations.transform(setup_label = strip) |> DataFrame
 
     setuplog = similar(df[1:1,:])
@@ -246,7 +246,7 @@ function dom_handler(sr::SkyRoom2, left2upload, session, request)
     restart(sr)
 
     md = Dict()
-    setup_file = HTTP.get(setupsurl["skyroom2"], retry = 4).body
+    setup_file = HTTP.get(setupsurl["skyroom2"]).body
     df = CSV.File(setup_file, header = 1:2, types = Dict(1 => String))  |> TableOperations.transform(setup_label = strip) |> DataFrame
     setups = select(df, :setup_label => identity => :label, r"star" => ByRow(parse2stars ∘ tuple) => :stars)
     options = collect(df.setup_label)
